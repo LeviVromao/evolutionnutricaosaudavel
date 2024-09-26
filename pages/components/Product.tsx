60e0b653b207443b303a52c1c0b437d7214abe30
NoOneLevi <zxvitor1@hotmail.com>
2024-09-26 18:04:01 -0300

    sitezin

--- pages/components/Product.tsx
+++ pages/components/Product.tsx
@@ -1,10 +1,13 @@
 import { IProductsProps } from "@/services/interfaces";
 import Image from "next/image";
 
-export default function ProductComponent(product: {data: IProductsProps[]}) {
+export default function ProductComponent({data = []}: { data: IProductsProps[]}) {
+  if(!data.length) {
+    return <div>Nenhum produto encontrado.</div>
+  }
   return (
     <>
-      {product.data.map((prod, index) => (
+      {data.map((prod, index) => (
         <div key={index} className="flex font-suse flex-col items-center text-left w-3/4 hover:shadow-lg p-2 duration-600 transition-all">
           <div className="flex items-center flex-col">
             <Image src={prod.image} alt="" width={260} height={260} />
